c6eae2c2da856288867b6c1278251a34943f8d0d
wjhe2 <wjhe2@wisc.edu>
2024-12-05 23:51:16 -0600

    commited new tests, slight modifications to mkfs and wfs

--- solution/wfs.c
+++ solution/wfs.c
@@ -4,11 +4,7 @@
 #include <errno.h>
 
 static int wfs_getattr(const char *path, struct stat *stbuf) {
-    // Implementation of getattr function to retrieve file attributes
-    // Fill stbuf structure with the attributes of the file/directory indicated by path
-    // ...
-
-    return 0; // Return 0 on success
+    
 }
 
 static int wfs_mknod(const char* path, mode_t mode, dev_t rdev) {
@@ -53,8 +49,8 @@ static struct fuse_operations ops = {
 
 
 int main(int argc, char *argv[]) {
-    // Initialize FUSE with specified operations
-    // Filter argc and argv here and then pass it to fuse_main
+    // Open mount point
+	int mount_fd = open(argv[argc - 1], )
 	
 
     return fuse_main(argc, argv, &ops, NULL);
