7ab3a3232a713bc961a758631f54c1ba078ee490
WILLIAM HE <wjh@snares-09.cs.wisc.edu>
2024-12-09 15:10:59 -0600

    bug fix 1

--- playground/test.sh
+++ playground/test.sh
@@ -7,7 +7,7 @@ cd ../solution
 make clean
 make
 
-./mkfs -r 1 -d disk1 -d disk2 -i 32 -b 200
+./mkfs -r 1 -d ../playground/disk1.img -d ../playground/disk2.img -i 32 -b 200
 echo "mkfs finished"
 valgrind ./wfs ../playground/disk1.img ../playground/disk2.img -f -s ../playground/mnt
 cd ../playground
